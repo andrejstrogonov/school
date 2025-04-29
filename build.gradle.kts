f30235b26b1b70a5217e5e93dcbfe23132655037
andrejstrogonov <strogonovandrej@hotmail.com>
2025-04-29 21:08:04 +0300

    Refactored liquibase file

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.postgresql:postgresql:42.7.5")
+	implementation("org.liquibase:liquibase-core:4.31.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
